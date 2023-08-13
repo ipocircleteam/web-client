901c900ff916146d4edb40d073ff3fe30dcfaae8
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-08-13 16:37:17 +0530

    reduce font size, add white space, and optimize ui

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -6,18 +6,20 @@ import LargeCTAbutton from '../Buttons/large-cta-btn'
 
 export default function Hero() {
   return (
-    <div className="my-6 md:my-0 md:mt-[25px] text-center">
-      <span className="flex justify-center items-center w-[95vw] mx-auto">
-        <h1 className="text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] font-bold leading-[1] ">
+    <div className="my-[50px] md:my-[35px] md:mb-[35px] text-center">
+      <span className="flex justify-center items-center w-[85vw] sm:w-[95vw] mx-auto">
+        <h1 className="text-[2rem] md:text-[3rem] lg:text-[3.2rem] font-bold leading-[1] ">
           <label className="text-primary">India's first </label>
           <label className="text-secondary">online grey market</label>
         </h1>
       </span>
 
-      <p className="my-4 md:my-2 text-[1.1rem] md:text-[1.3rem] leading-tight text-primary w-[95vw] mx-auto">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a tortor
-        leo. Integer <br /> interdum dolor neque, vitae consectetur elit
-        interdum vel.
+      <p
+        className="my-4 md:my-2 text-[1rem] md:text-[1.3rem] leading-tight text-primary 
+      w-[90vw] sm:w-[95vw] mx-auto"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing <br />
+        elit. Sed a tortor leo.
       </p>
       <div className="my-4">
         <LargeCTAbutton text="Free Consulting" function={() => {}} />
