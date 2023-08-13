59f8446e36365a4277afca7be278007e86b8ccbc
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-08-13 10:17:09 +0530

    formatting

--- src/hooks/useWindowWidth.tsx
+++ src/hooks/useWindowWidth.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import useEventListener from './useEventListener'
 
-const isClient = typeof window === 'object';
+const isClient = typeof window === 'object'
 
 export default function useWindowWidth() {
   const [width, setWidth] = useState(isClient ? window.innerWidth : 0)
-  
-  if (!isClient) return;
+
+  if (!isClient) return
 
   useEventListener('resize', () => {
     setWidth(window.innerWidth)
