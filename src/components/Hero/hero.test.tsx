6e2b3f98e23c4550defe2077ce9a0a6a233bb67c
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-08-13 12:26:48 +0530

    add test file for home hero

---
+++ src/components/Hero/hero.test.tsx
@@ -0,0 +1,19 @@
+import { screen, render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Hero from './hero'
+
+describe('home page hero renders correctly', () => {
+  test('heading renders correctly', () => {
+    render(<Hero />)
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument()
+  })
+
+  test('button renders correctly', () => {
+    render(<Hero />)
+    const buttonEl = screen.getByRole('button', {
+      name: 'Free Consulting',
+    })
+    expect(buttonEl).toBeInTheDocument()
+  })
+})
