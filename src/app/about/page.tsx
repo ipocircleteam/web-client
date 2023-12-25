f722c919e53a94c973fa23135cc9923495644a08
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-12-25 19:29:20 +0530

    fix issue 68 - remove team ipo details

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -67,7 +67,7 @@ export default async function About() {
           probability of profit.
         </p>
 
-        <div>
+        {/* <div>
           <h1 className="text-[20px] text-bold text-black mt-[40px]">
             Team IPO Circle
           </h1>
@@ -102,7 +102,7 @@ export default async function About() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
         <br />
         <p className="font-bold text-[18px] mb-[50px]">
